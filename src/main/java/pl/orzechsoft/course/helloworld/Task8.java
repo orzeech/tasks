8d93b203f9c956f7dfdf40599fd60d94e1a6c63c
Wojciech Jarosz <wojtek_j@o2.pl>
2020-10-15 20:39:13 +0200

    Implement task 8

--- src/main/java/pl/orzechsoft/course/helloworld/Task8.java
+++ src/main/java/pl/orzechsoft/course/helloworld/Task8.java
@@ -3,7 +3,6 @@ package pl.orzechsoft.course.helloworld;
 public class Task8 {
 
   public static void main(String[] args) {
-    System.out.println("Whatever");
+    System.out.println("Cokolwiek");
   }
-
 }
