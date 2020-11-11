2b10f00d4ba7c262cae50e88b7af6edf844bfd51
Wojciech Jarosz <wojtek_j@o2.pl>
2020-11-11 13:16:09 +0100

    Correct simulation for task 16

--- src/main/java/pl/orzechsoft/course/task16/Simulation.java
+++ src/main/java/pl/orzechsoft/course/task16/Simulation.java
@@ -11,8 +11,12 @@ import pl.orzechsoft.course.task16.example.ExampleCar;
 public class Simulation {
 
   public static void main(String[] args) {
-
     List<Integer> track = List.of(200, 1000, 100, 400, 300, 200, 50, 50, 50, 3000);
+    List<Integer> track2 = List.of(10000, 10000);
+    List<Integer> track3 = List
+        .of(2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+            2, 2, 2, 2, 2, 2, 2);
+    List<Integer> track4 = List.of(10, 10, 10, 10, 10);
 
     RaceCar car = new ExampleCar();
     car.prepareForRace(track);
@@ -26,33 +30,64 @@ public class Simulation {
   }
 
   private static List<RaceCar> simulate(List<RaceCar> cars, List<Integer> track) {
-    Map<RaceCar, Integer> results = new HashMap<>();
+    Map<RaceCar, Double> results = new HashMap<>();
     for (RaceCar car : cars) {
-      double currentSpeed = 0;
-      int secondsElapsed = 0;
-      for (int straightPart : track) {
-        System.out.println("Started " + straightPart + "m long part");
-        double currentPartDone = 0;
-        while (currentPartDone < straightPart) {
-          if (car.getTyres().getMaxTurnSpeed() > car.getBrakes().getDeceleration() * currentSpeed) {
-            currentSpeed = Math.min(
-                car.getEngine().getMaxSpeed() * (1 - car.getTransmission().getMaxSpeedPenalty()),
-                currentSpeed + car.getTransmission().getAcceleration() * car.getEngine()
-                    .getAccelerationPenalty());
-          } else {
-            currentSpeed -= Math.max(0,
-                car.getBrakes().getDeceleration() * car.getEngine().getAccelerationPenalty());
-          }
-          currentPartDone += currentSpeed;
-          secondsElapsed++;
-        }
+      System.out.println("Car " + car.getName() + " starting");
+      if (car.getBrakes().getUses() < track.size()) {
+        System.out
+            .println("Car " + car.getName() + " crashed because it couldn't brake after " + car
+                .getBrakes().getUses() + " turns");
+        continue;
       }
-      results.put(car, secondsElapsed);
+      double maxSpeed =
+          car.getEngine().getMaxSpeed() / 3.6 * (1 - car.getTransmission().getMaxSpeedPenalty());
+      double acceleration = car.getTransmission().getAcceleration() * car.getEngine()
+          .getAccelerationPenalty();
+      double deceleration =
+          -1 * car.getBrakes().getDeceleration() * car.getEngine().getAccelerationPenalty();
+      double currentSpeed = 0;
+      double maxTurnSpeed = car.getTyres().getMaxTurnSpeed() / 3.6;
+      double totalTime = 0;
+      for (int straightPart : track) {
+        double t;
+        System.out.println("Car " + car.getName() + " started " + straightPart + "m long part");
+        boolean canReachTurnSpeed = currentSpeed >= maxTurnSpeed
+            || (maxTurnSpeed * maxTurnSpeed - currentSpeed * currentSpeed) / (2 * acceleration)
+            < straightPart;
+        if (!canReachTurnSpeed) {
+          t = Math.sqrt(2 * straightPart / acceleration);
+          currentSpeed += acceleration * t;
+        } else {
+          double v = Math.sqrt(Math.abs((2 * straightPart * acceleration * deceleration
+              + deceleration * currentSpeed * currentSpeed
+              - acceleration * maxTurnSpeed * maxTurnSpeed) / (deceleration - acceleration)));
+          if (v > maxSpeed) {
+            double deltaV = maxSpeed - currentSpeed;
+            double t1 = deltaV / acceleration;
+            double deltaV2 = maxSpeed - maxTurnSpeed;
+            double t2 = Math.abs(deltaV2 / deceleration);
+            double partAtConstantSpeed =
+                0.5 * acceleration * t1 * t1 + 0.5 * Math.abs(deceleration) * t2 * t2;
+            double t3 = partAtConstantSpeed / maxSpeed;
+            t = t1 + t2 + t3;
+          } else {
+            double deltaV = v - currentSpeed;
+            double t1 = deltaV / acceleration;
+            double deltaV2 = v - maxTurnSpeed;
+            double t2 = Math.abs(deltaV2 / deceleration);
+            t = t1 + t2;
+          }
+          currentSpeed = Math.min(v, maxTurnSpeed);
+        }
+        totalTime += t;
+      }
+      totalTime = totalTime + 10 * (track.size() / car.getTyres().getMaxTurns());
+      results.put(car, totalTime);
       System.out
-          .println("Car " + car.getName() + " has beaten the track in " + secondsElapsed + "s");
+          .println("Car " + car.getName() + " has beaten the track in " + totalTime + "s");
     }
 
-    return results.entrySet().stream().sorted(Comparator.comparingInt(Entry::getValue))
+    return results.entrySet().stream().sorted(Comparator.comparingDouble(Entry::getValue))
         .map(Entry::getKey).collect(
             Collectors.toList());
   }
